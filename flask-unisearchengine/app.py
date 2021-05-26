8f81be88f17625e6939a731634397cf0a8f7c8ca
edisoniver <edisoniverbotor@gmail.com>
2021-05-26 14:29:00 +1000

    Flask Development added

---
+++ flask-unisearchengine/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, json, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    title = 'THIS IS A TITLE'
+    return render_template("index.html", title = title)
+
+
+app.run(host="0.0.0.0", port = 5000, debug = True)
